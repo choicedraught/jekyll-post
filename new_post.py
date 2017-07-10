945803924560851defe3b9945603e44fa365d9f9
choicedraught <jack.bryant@gmail.com>
2017-07-10 09:54:19 +0800

    Added categories argument

--- new_post.py
+++ new_post.py
@@ -15,12 +15,14 @@ from datetime import datetime, timedelta, timezone
 import time
 
 ArgumentParser = argparse.ArgumentParser(description='Give me the title of your Blog post mofo.')
-ArgumentParser.add_argument('title', nargs='+', help='Required: The Title of your Blog Post') # This is how you do a positional argument apparently
+ArgumentParser.add_argument('title', nargs='+', help='Required: The Title of your Post') # This is how you do a positional argument apparently
+ArgumentParser.add_argument('categories', nargs='+', help='Required: The Categories for your Post')
 ArgumentParser.add_argument('--datetime', required=False, dest='datetime',help='Optional: specity a post date that is not today.  Format YYYY-MM-DD HH:MM:SS +/-0000')
 
-date      = datetime.now(timezone.utc).astimezone()
-args      = ArgumentParser.parse_args()
-title     = args.title
+date         = datetime.now(timezone.utc).astimezone()
+args         = ArgumentParser.parse_args()
+title        = args.title
+categories   = " ".join(args.categories)
 
 if not args.datetime:
   shortdate  = date.strftime("%Y-%m-%d")
@@ -30,8 +32,9 @@ else:
   shortdate  = isodate.split(" ",2)[0] # We just want the date part for out filename
 
 filetitle    = "-".join(title) # Split the title into words and then hyphen them
+posttitle    = " ".join(title)
 filename     = shortdate+"-"+filetitle+".markdown" # Now create the filename 
-output       = "---\nlayout: post\ntitle:  "+" ".join(title)+"\ndate:   "+isodate+"\ncategories: jekyll update\n---\n" # Create the Post conetent as a string
+output       = "---\nlayout: post\ntitle:  "+posttitle+"\ndate:   "+isodate+"\ncategories: "+categories+"\n---\n" # Create the Post conetent as a string
 
 # Check if the file exists first
 if os.path.isfile(filename):

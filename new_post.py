f9e81f736ee53917e030f71f99e7b558d9b5f48e
choicedraught <jack.bryant@gmail.com>
2017-07-10 08:58:19 +0800

    Update the date code to something more sensible which python3 allows

--- new_post.py
+++ new_post.py
@@ -11,16 +11,10 @@
 
 import argparse
 import os.path
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import time
-def date2iso(thedate): # Some dudes neat little hack to get the TZ Offset
-     strdate = thedate.strftime("%Y-%m-%d %H:%M:%S")
-     minute = (time.localtime().tm_gmtoff / 60) % 60
-     hour = ((time.localtime().tm_gmtoff / 60) - minute) / 60
-     utcoffset = "%.2d%.2d" %(hour, minute)
-     if utcoffset[0] != '-':
-         utcoffset = ' +' + utcoffset
-     return strdate + utcoffset
+
+date = datetime.now(timezone.utc).astimezone()
 
 ArgumentParser = argparse.ArgumentParser(description='Give me the title of your Blog post mofo.')
 ArgumentParser.add_argument('title', nargs='+', help='Required: The Title of your Blog Post') # This is how you do a positional argument apparently
@@ -30,31 +24,27 @@ args      = ArgumentParser.parse_args()
 title     = args.title
 
 if not args.datetime:
-  mydate  = datetime.now().strftime("%Y-%m-%d")
-  isodate = date2iso(datetime.fromtimestamp(time.time()))
+  shortdate  = date.strftime("%Y-%m-%d")
+  isodate = date.strftime('%Y-%m-%d %H:%M:%S %z')
 else:
   isodate = args.datetime
-  mydate  = isodate.split(" ",2)[0] # We just want the date part for out filename
+  shortdate  = isodate.split(" ",2)[0] # We just want the date part for out filename
 
 filetitle = "-".join(title) # Split the title into words and then hyphen them
-filename  = mydate+"-"+filetitle+".markdown" # Now create the filename 
+filename  = shortdate+"-"+filetitle+".markdown" # Now create the filename 
 output    = "---\nlayout: post\ntitle:  "+" ".join(title)+"\ndate:   "+isodate+"\ncategories: jekyll update\n---\n" # Create the Post conetent as a string
-
-# Debugging
-#print(title)
-#print(mydate)
-#print(isodate)
-#print(filetitle)
 
 # Check if the file exists first
 if os.path.isfile(filename):
   print("\n")  
   print("Error: File '"+filename+"'"+" exists, exiting.")
-  print("\n")  
+  print("\n") 
+  print(output) 
   quit()
 else:
-  f = open(filename, 'w')
-  f.write(output)
+  print(output)
+  #f = open(filename, 'w')
+  #f.write(output)
 
 print("\n")  
 print("Created new blog post in: '"+filename+"'. You're welcome!\n")
